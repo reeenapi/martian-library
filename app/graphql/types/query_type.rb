ee3d378fcd7c9308343ac904a33ad837c5880fa2
Rena Hamada <renahamada0221@gmail.com>
2019-08-29 20:38:01 +0900

    feat: add items on query type

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,5 +9,11 @@ module Types
     def test_field
       "Hello World!"
     end
+
+    field :items, [Types::ItemType], null: false,
+      description: "Returns a list of items in the martian library"
+    def items
+      Item.all
+    end
   end
 end
